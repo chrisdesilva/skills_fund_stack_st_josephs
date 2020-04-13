1d79f3342226b54115665285cf53bd2384ac7e5c
Chris DeSilva <chris@skills.fund>
2020-04-13 16:31:59 -0500

    Add deposit note, update headline

--- src/constants/programInfo.js
+++ src/constants/programInfo.js
@@ -100,7 +100,7 @@ export const schoolURL = "https://www.SCHOOLWEBSITE.com/" // update with url of 
 
 export const skfURL = "https://stjosephs.skills.fund" // update with Skills Fund url
 
-export const headline = "Learn to Code at St. Joseph's College" // update headline as appropriate
+export const headline = "Learn New Skills at St. Joseph's College" // update headline as appropriate
 
 export const leadContent = {
   header: "Your last step on the path toward changing your career",
